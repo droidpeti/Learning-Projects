7987b26320bbb7b7a941c304de4fd3f46821989f
Ballai Péter <ballaipeterlorant@gmail.com>
2026-02-01 20:20:24 +0100

    Updated rust multiplication table project

--- Rust/mult_table/src/main.rs
+++ Rust/mult_table/src/main.rs
@@ -1,35 +1,31 @@
 use std::io;
-use std::io::Write;
-use std::process;
 
 fn main() {
     println!("Welcome to the Multiplication Table program!");
     let rows = read_int("rows");
     let cols = read_int("cols");
+    let cell_width = (rows*cols).to_string().len() + 1;
 
-    let mut i = 1;
-    while i <= rows{
-        let mut j = 1;
-        while j <= cols{
-            print!("{} ", i*j);
-            io::stdout().flush().unwrap();
-            j += 1;
+    for i in 1..=rows {
+        for j in 1..=cols {
+            print!("{:cell_width$}", i * j); 
         }
         println!();
-        i += 1;
     }
 }
 
 fn read_int(name: &str) -> u32 {
-    println!("Please enter {name}: ");
-    let mut num = String::new();
-    io::stdin()
-        .read_line(&mut num)
-        .expect("Failed to read {name}!");
+    loop {
+        println!("Please enter {name}: ");
+        let mut num = String::new();
+        
+        io::stdin()
+            .read_line(&mut num)
+            .expect("Failed to read input");
 
-    let num : u32 = match num.trim().parse() {
-        Ok(num) => num,
-        Err(_) => process::exit(1),
-    };
-    num
+        match num.trim().parse() {
+            Ok(n) => return n,
+            Err(_) => println!("Invalid input, please enter a positive number."),
+        }
+    }
 }
